4353c158af38eab097b5dcbbee80aee8639a35c4
ducharme <48138912+Ducharmee@users.noreply.github.com>
2022-03-30 14:39:02 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,50 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
-	}
+            if (num % 15 == 0){
+          return "Fizz Buzz";
+        } else if(num % 3 == 0){
+          return "Fizz";
+        } else if (num % 5 == 0){
+          return "Buzz";
+        }
+        return Integer.toString(num);
+    }
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
-	}
+         String result = "";
+        if(nums.length == 1){
+          return processNumber(nums[0]);
+        } else if (nums.length == 0){
+          return "";
+        }
+        int i;
+        for(i = 0; nums.length > i; i++){
+          if(i == nums.length - 1){
+            if (nums[i] % 15 == 0){
+              result = result + "Fizz Buzz";
+            } else if(nums[i] % 3 == 0){
+              result = result + "Fizz";
+            } else if (nums[i] % 5 == 0){
+              result = result + "Buzz";
+            } else {
+              result = result + Integer.toString(nums[i]) + ",";
+            }
+          } else { 
+            if (nums[i] % 15 == 0){
+                result = result + "Fizz Buzz,";
+              } else if(nums[i] % 3 == 0){
+                result = result + "Fizz,";
+              } else if (nums[i] % 5 == 0){
+                result = result + "Buzz,";
+              } else {
+                result = result + Integer.toString(nums[i]) + ",";
+              }
+          }
+        }
+        return result;
+    }
+    
 
 }
