f728eeb7f45d3ebd23ae27bfa07a5f27bb61b46e
ducharme <48138912+Ducharmee@users.noreply.github.com>
2022-03-30 14:49:45 -0400

    Redo

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -2,52 +2,30 @@ package com.demo;
 
 public class FizzBuzz 
 {
-    String processNumber(int num)
-	{	
-            if (num % 15 == 0){
-          return "Fizz Buzz";
-        } else if(num % 3 == 0){
+    String processNumber(int num) {
+      if ( (num % 15 == 0) ) {
+          return "FizzBuzz";
+        } else if (num % 3 == 0) {
           return "Fizz";
-        } else if (num % 5 == 0){
+        } else if (num % 5 == 0) {
           return "Buzz";
+        } else {
+          return num + "";
         }
-        return Integer.toString(num);
-    }
-	
-	String processNumbers (int[] numList)
-	{
-         String result = "";
-        if(numList.length == 1){
-          return processNumber(numList[0]);
-        } else if (numList.length == 0){
-          return "";
-        }
-        int i;
-        for(i = 0; numList.length > i; i++){
-          if(i == numList.length - 1){
-            if (numList[i] % 15 == 0){
-              result = result + "Fizz Buzz";
-            } else if(numList[i] % 3 == 0){
-              result = result + "Fizz";
-            } else if (numList[i] % 5 == 0){
-              result = result + "Buzz";
-            } else {
-              result = result + Integer.toString(numList[i]) + ",";
-            }
-          } else { 
-            if (numList[i] % 15 == 0){
-                result = result + "Fizz Buzz,";
-              } else if(numList[i] % 3 == 0){
-                result = result + "Fizz,";
-              } else if (numList[i] % 5 == 0){
-                result = result + "Buzz,";
-              } else {
-                result = result + Integer.toString(numList[i]) + ",";
-              }
-          }
-        }
-        return result;
+
     }
     
+    String processNumbers (int[] numList) {
+        
+        int len = numList.length, i;
+        String finalRes = "";
+        for (i = 0; i < len-1; i++){
+          finalRes += processNumber(numList[i]) + ", ";
+        }
+        return finalRes + processNumber(numList[len-1]);
+    }
+
+}
+	   
 
 }
